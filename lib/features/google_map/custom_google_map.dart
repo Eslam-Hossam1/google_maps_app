c650483a33518a5599c1f16dbe501b0fcf5e13cf
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-14 20:17:00 +0300

    feat: add controller to google map and animate camera

--- lib/features/google_map/custom_google_map.dart
+++ lib/features/google_map/custom_google_map.dart
@@ -11,6 +11,13 @@ class CustomGoogleMap extends StatefulWidget {
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition cameraPosition;
   late CameraTargetBounds cameraTargetBounds;
+  late GoogleMapController googleMapController;
+  @override
+  void dispose() {
+    super.dispose();
+    googleMapController.dispose();
+  }
+
   @override
   void initState() {
     super.initState();
@@ -31,9 +38,28 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      cameraTargetBounds: cameraTargetBounds,
-      initialCameraPosition: cameraPosition,
+    return Stack(
+      children: [
+        GoogleMap(
+          //  cameraTargetBounds: cameraTargetBounds,
+          onMapCreated: (controller) => googleMapController = controller,
+          initialCameraPosition: cameraPosition,
+        ),
+        Positioned(
+          bottom: 36,
+          left: 50,
+          right: 50,
+          child: ElevatedButton(
+            onPressed: () {
+                googleMapController.animateCamera(
+                  CameraUpdate.newLatLng(
+                      LatLng(30.05711214641764, 31.303165496389212)),
+               );
+            },
+            child: Text('change postion'),
+          ),
+        ),
+      ],
     );
   }
 }
