9f687bcdb15f1df22dc54715ebd03776930ac14c
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-17 00:08:27 +0300

    feat: add sharqiaLatLngs hole to egyptPolygon

--- lib/features/google_map/presentation/views/widgets/custom_google_map.dart
+++ lib/features/google_map/presentation/views/widgets/custom_google_map.dart
@@ -39,23 +39,37 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   }
 
   initPolyLines() {
+    //to be added to egypt Polygon as hole
+    List<LatLng> sharqiaLatLngs = [
+      LatLng(30.8500, 31.6000), // شمال غربي قريب من القليوبية
+      LatLng(30.9500, 31.8500), // شمال عند الدقهلية
+      LatLng(31.1500, 31.9000), // شمال شرقي ناحية المنزلة
+      LatLng(30.9000, 32.0000), // شرق قريب من الإسماعيلية
+      LatLng(30.6000, 32.0500), // جنوب شرق قريب من السويس
+      LatLng(30.3000, 31.9000), // جنوب قريب من القاهرة
+      LatLng(30.4000, 31.6000), // جنوب غربي
+      LatLng(30.7000, 31.5000), // غرب (بلبيس – العاشر من رمضان)
+    ];
+
     //polygon is any 2-d shape
     Polygon egyptPolygon = Polygon(
-      polygonId: PolygonId('1'),
-      points: [
-        LatLng(31.59873996440776, 25.091298804499562),
-        LatLng(31.56165580486618, 30.924414292410454),
-        LatLng(31.264452333541957, 34.22404230723502),
-        LatLng(29.54090335472602, 34.886879763893035),
-        LatLng(28.104223304617385, 34.550115425233166),
-        LatLng(22.2916725000781, 37.68348796754582),
-        LatLng(22.006883023279123, 38.1081038728126),
-        LatLng(22.006883023279123, 25.003578520613683),
-      ],
-      fillColor: Colors.pink.withAlpha(50),
-      strokeColor: Colors.cyan,
-      strokeWidth: 5,
-    );
+        polygonId: PolygonId('1'),
+        points: [
+          LatLng(31.59873996440776, 25.091298804499562),
+          LatLng(31.56165580486618, 30.924414292410454),
+          LatLng(31.264452333541957, 34.22404230723502),
+          LatLng(29.54090335472602, 34.886879763893035),
+          LatLng(28.104223304617385, 34.550115425233166),
+          LatLng(22.2916725000781, 37.68348796754582),
+          LatLng(22.006883023279123, 38.1081038728126),
+          LatLng(22.006883023279123, 25.003578520613683),
+        ],
+        fillColor: Colors.pink.withAlpha(50),
+        strokeColor: Colors.cyan,
+        strokeWidth: 5,
+        holes: [
+          sharqiaLatLngs,
+        ]);
     polygons.add(egyptPolygon);
   }
 
