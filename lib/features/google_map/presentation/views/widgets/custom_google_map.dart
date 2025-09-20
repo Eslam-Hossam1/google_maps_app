a82090f96e74bce6a813517c3f8fe1406918d3a6
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-20 21:35:54 +0300

    feat: getLocationAndAnimateCamera and refactor CustomGoogleMap

--- lib/features/google_map/presentation/views/widgets/custom_google_map.dart
+++ lib/features/google_map/presentation/views/widgets/custom_google_map.dart
@@ -17,13 +17,11 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   String? nightMapStyle;
   late LocationService locationService;
   Set<Marker> markers = {};
-  bool isFirstLocationCall = true;
   @override
   void initState() {
     super.initState();
     cameraPosition = CameraPosition(
-      target: LatLng(31.04093837052159, 31.379579171401787),
-      zoom: 1,
+      target: LatLng(0, 0),
     );
     loadGoogleMapStyle();
     locationService = LocationService();
@@ -39,6 +37,7 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   void dispose() {
     super.dispose();
     googleMapController?.dispose();
+    locationService.dispose();
   }
 
   @override
@@ -49,49 +48,43 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
       initialCameraPosition: cameraPosition,
       onMapCreated: (controller) {
         googleMapController = controller;
-        locationService.listenToLiveLocationChanges(onLocationChanged);
+        getLocationAndAnimateCamera();
       },
     );
   }
 
-  void onLocationChanged(LocationData locationData) {
+  getLocationAndAnimateCamera() async {
+    LocationData locationData = await locationService.getLocationData();
     LatLng newLatLng = LatLng(
       locationData.latitude!,
       locationData.longitude!,
     );
-    setNewLocationMarker(newLatLng);
-    animateCameraToNewLocation(newLatLng);
+    setLocationMarker(newLatLng);
+    animateCameraToLocation(newLatLng);
   }
 
-  void animateCameraToNewLocation(LatLng newLatLng) {
-    if (isFirstLocationCall) {
-      isFirstLocationCall = false;
-      CameraPosition newCameraPostion =
-          CameraPosition(target: newLatLng, zoom: 15);
-      googleMapController?.animateCamera(
-        CameraUpdate.newCameraPosition(
-          newCameraPostion,
-        ),
-      );
-    } else {
-      googleMapController?.animateCamera(
-        CameraUpdate.newLatLng(
-          newLatLng,
-        ),
-      );
-    }
+  void animateCameraToLocation(LatLng newLatLng) {
+    CameraPosition newCameraPostion =
+        CameraPosition(target: newLatLng, zoom: 16);
+    googleMapController?.animateCamera(
+      CameraUpdate.newCameraPosition(
+        newCameraPostion,
+      ),
+    );
   }
 
-  void setNewLocationMarker(LatLng newLatLng) {
-    markers = {
+  void setLocationMarker(LatLng newLatLng) {
+    markers.add(
       Marker(
         markerId: const MarkerId('new_postion_marker'),
         position: newLatLng,
       ),
-    };
+    );
     setState(() {});
   }
 }
+
+  
 
 // inquire about location service 
 // rquestlocation permission
